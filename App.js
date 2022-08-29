9f57f7a3b1b4a199a79bb7192a3310e608c829d0
Zakaria Kerkeb <zkerkeb@student.42.fr>
2022-08-29 03:21:22 +0200

    Base + composant + es6 + cycle de vie des composant

---
+++ App.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import Home from './src/screens/home';
+import Practice from './src/screens/practice';
+
+const App = () => {
+  return <Practice />;
+};
+
+export default App;
